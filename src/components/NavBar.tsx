cc9f09d8cc64cbb6f745de2dd61db7728d97c486
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-05-09 14:26:25 +0530

    converted navbar to client side component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,30 +1,30 @@
-import { auth, signIn, signOut } from "@/auth";
+"use client";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React from "react";
 
-function SignIn() {
+const NavBarComponent = () => {
+  const session = useSession();
+  const router = useRouter();
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signIn();
-      }}
-    >
-      <button type="submit">Sign In</button>
-    </form>
-  );
-}
-
-const NavBarComponent = async () => {
-  const session = await auth();
-  return (
-    <nav className="flex w-full items-center justify-between border-b-2 border-gray-300 p-5 text-xl font-semibold bg-[#2586ed] text-white ">
+    <nav className="flex w-full items-center justify-between border-b-2 border-gray-700 p-5 text-xl font-semibold bg-[#2586ed] text-white ">
       <div className="text-2xl">Blockchain Crowdfunding</div>
       <div className="flex flex-row items-center gap-4">
         <Link href={"/about"}>About</Link>
         <Link href={"/my-campaigns"}>My Campaigns</Link>
-        {session?.user && <Link href={"/logout"}>Sign Out</Link>}
-        {!session?.user && <SignIn />}
+        {session?.data?.user && (
+          <Image
+            src={session.data.user.image as string}
+            alt="Avatar"
+            width={50}
+            height={50}
+            className="rounded-[100px] border-gray-700 border-2 cursor-pointer"
+            onClick={() => router.push("/profile")}
+          />
+        )}
+        {!session?.data?.user && <Link href={"/login"}>Sign In</Link>}
       </div>
     </nav>
   );
