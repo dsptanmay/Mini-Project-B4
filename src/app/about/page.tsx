b4acd9730ee4d64f33b56fea69e0235dadd719fb
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-05-13 16:20:10 +0530

    updated about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
+    <div className="h-screen w-screen dark:bg-black dark:bg-dot-white/[0.2] bg-white bg-dot-black/[0.2] p-5">
+      <h1 className="font-bold text-5xl text-center text-white">About</h1>
+    </div>
+  );
+};
 
-export default AboutPage
+export default AboutPage;
